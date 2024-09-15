11f1918659afce73926b0c0ddff610ada42cab65
Ivan <133674171+EvanQa@users.noreply.github.com>
2024-09-15 17:22:31 +0300

    Add files via upload

---
+++ Homework5/Exercise1.py
@@ -0,0 +1,8 @@
+
+time: int = int(input("enter a time"))
+hour: int = time // 60
+minutes: int = time % 60
+
+print(f"video length in hour is: {hour}")
+print(f"minutes is: {minutes}")
+
---
+++ Homework5/Exercise2.py
@@ -0,0 +1,20 @@
+
+for x in range(0, 10 + 1):
+    print(x, end=" ")
+
+print()
+
+
+for x in range(40, 88 + 1, 2):
+    print(x, end=" ")
+
+print()
+
+for i in range(77,  105 + 1, 7):
+    print(i, end=" ")
+
+print()
+
+for i in range(99, 66 - 1, - 3 ):
+    print(i, end=" ")
+
